a47edc750d8df5e9ed68a0853a0d9da17455b90a
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-03-18 17:50:55 +0500

    Modified App.js to load cached ayahs

--- MyReactNativeApp/App.js
+++ MyReactNativeApp/App.js
@@ -1,25 +1,68 @@
-// After navigation
-import React from "react";
+// // After navigation
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import HomeScreen from "./screens/HomeScreen";
+// import QuranScreen from "./screens/QuranScreen";
+
+// const Stack = createStackNavigator();
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{ title: "Home" }}
+//         />
+//         <Stack.Screen
+//           name="Quran"
+//           component={QuranScreen}
+//           options={{ title: "Quran" }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+//Modified App.js to load cached ayahs
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import HomeScreen from "./screens/HomeScreen";
 import QuranScreen from "./screens/QuranScreen";
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const [cachedAyahs, setCachedAyahs] = useState(null);
+
+  useEffect(() => {
+    const loadCachedData = async () => {
+      try {
+        const storedAyahs = await AsyncStorage.getItem("ayahs");
+        if (storedAyahs) {
+          setCachedAyahs(JSON.parse(storedAyahs));
+        }
+      } catch (error) {
+        console.error("Error loading cached ayahs:", error);
+      }
+    };
+    loadCachedData();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Home" }}
-        />
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen
           name="Quran"
           component={QuranScreen}
-          options={{ title: "Quran" }}
+          initialParams={{ ayahs: cachedAyahs }}
         />
       </Stack.Navigator>
     </NavigationContainer>
